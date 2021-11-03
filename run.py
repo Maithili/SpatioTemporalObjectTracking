b93b7590ab006335ad1b74ce0c36f6ff0c38b9e9
Maithili Patel <maithili@gatech.edu>
2021-11-03 18:16:45 -0400

    Moved to python scripts

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+#!/home/maithili/repos/GraphTrans/.venv/bin/python
+
 import yaml
 import os
+import sys
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
 
@@ -8,24 +11,30 @@ from reader import RoutinesDataset
 from encoders import time_encoding_options
 from analyzers import loss_options
 
-def run(cfg):
+DEFAULT_CONFIG = 'config/default.yaml'
+
+def run(cfg_in):
+    with open(DEFAULT_CONFIG) as f:
+        cfg = yaml.safe_load(f)
+    cfg.update(cfg_in)
+
     try:
-        wandb_logger = WandbLogger(name=cfg['MODEL']['NAME'])
+        wandb_logger = WandbLogger(name=cfg['NAME'])
     except:
         wandb_logger = WandbLogger()
     wandb_logger.experiment.config.update(cfg)
     
-    assert cfg['MODEL']['TIME_ENCODING'] in time_encoding_options, 'Time encoding {} specified in config should be one of {}'.format(cfg['MODEL']['TIME_ENCODING'], time_encoding_options.keys())
-    time_encoding = time_encoding_options[cfg['MODEL']['TIME_ENCODING']]
+    assert cfg['TIME_ENCODING'] in time_encoding_options, 'Time encoding {} specified in config should be one of {}'.format(cfg['TIME_ENCODING'], time_encoding_options.keys())
+    time_encoding = time_encoding_options[cfg['TIME_ENCODING']]
 
-    data = RoutinesDataset(data_path=cfg['DATA_PATH']['GRAPHS'], classes_path=cfg['DATA_PATH']['CLASSES'], time_encoder=time_encoding, test_perc=cfg['TRAIN']['TEST_SPLIT'])
+    data = RoutinesDataset(data_path=cfg['DATA_PATH'], classes_path=cfg['CLASSES_PATH'], time_encoder=time_encoding, test_perc=cfg['TEST_SPLIT'])
     
     losses = loss_options(data)
 
-    assert cfg['TRAIN']['LOSS'] in losses, 'Loss {} specified in config should be one of {}'.format(cfg['TRAIN']['LOSS'], losses.keys())
-    train_loss = losses(cfg['TRAIN']['LOSS'])
+    assert cfg['LOSS'] in losses, 'Loss {} specified in config should be one of {}'.format(cfg['LOSS'], losses.keys())
+    train_loss = losses(cfg['LOSS'])
     logging_loss_funcs = []
-    for logging_loss in cfg['TRAIN']['LOSSES_LOG']:
+    for logging_loss in cfg['LOSSES_LOG']:
         assert logging_loss in losses, 'Loss {} specified in config should be one of {}'.format(logging_loss, losses.keys())
         logging_loss_funcs.append(losses(logging_loss))
 
@@ -36,15 +45,18 @@ def run(cfg):
                               train_analyzer=train_loss, 
                               logging_analyzers=logging_loss_funcs)
 
-    trainer = Trainer(max_epochs=cfg['TRAIN']['EPOCHS'], logger=wandb_logger)
+    trainer = Trainer(max_epochs=cfg['EPOCHS'], logger=wandb_logger)
 
     trainer.fit(model, data.get_train_loader())
     trainer.test(model, data.get_test_loader())
 
-    wandb_logger.experiment.finish()
+    wandb_logger.experiment.finish(exit_code=0)
 
 def run_from_config(config_filename):
     with open(os.path.join('config',config_filename)+'.yaml') as f:
         cfg = yaml.safe_load(f)
     run(cfg)
 
+if __name__ == '__main__':
+    assert len(sys.argv) == 2, "The script requires exactly one argument specifying the config file name, e.g. 'sample'"
+    run_from_config(sys.argv[1])
